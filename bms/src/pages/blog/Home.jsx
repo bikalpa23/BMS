efa7c9f044286f9e913f2fb520362eab52ac27b7
Bikalpa Sangat <bikalpasangat1@gmail.com>
2024-04-16 11:33:25 +0545

    first commit

--- bms/src/pages/blog/Home.jsx
+++ bms/src/pages/blog/Home.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Navbar from '../../components/Navbar'
+import Layout from '../../components/layout/Layout'
+import Card from './components/card/Card'
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <Layout>
+      <div className='flex flex-wrap justify-center space-x-5 mt-3 mb-6'>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </Layout>
   )
 }
 
